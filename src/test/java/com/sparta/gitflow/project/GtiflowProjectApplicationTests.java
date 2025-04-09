faa9d0f1dc2a594751088d8bf5af47e5fbff8070
theco2de <theco2de@gmail.com>
2025-04-09 11:45:00 +0900

    feat: 프로젝트 최초 설정

---
+++ src/test/java/com/sparta/gitflow/project/GtiflowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.gitflow.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtiflowProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
