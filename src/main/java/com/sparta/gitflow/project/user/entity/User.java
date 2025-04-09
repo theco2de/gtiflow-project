43cd9de21c5b3395dca096bad9e1d522c40189df
theco2de <theco2de@gmail.com>
2025-04-09 12:10:50 +0900

    fix(user) : 팩토리 메소드명 수정

--- src/main/java/com/sparta/gitflow/project/user/entity/User.java
+++ src/main/java/com/sparta/gitflow/project/user/entity/User.java
@@ -29,7 +29,7 @@ public class User extends BaseEntity {
         this.id = id;
     }
 
-    public static User fromUserId(Long id) {
+    public static User from(Long id) {
         return new User(id);
     }
 
